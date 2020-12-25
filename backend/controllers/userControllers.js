c091d1a8f9f49e6c7e0fd35970fa3703f1257419
Saed Daoud <eng.saed.daoud@gmail.com>
2020-12-25 12:04:12 -0500

    added a custom error message when the password for a new rsgistered user doesn't meet the required coniditions without displaying the password in plain text

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -31,6 +31,7 @@ const registerUser = asyncHandler(async (req, res, next) => {
   const { email } = req.body;
   await User.findOne({ email }, async (err1, foundUser) => {
     if (err1) {
+      console.log(err1);
       next(err1);
     } else if (foundUser) {
       res.status(400);
@@ -38,7 +39,14 @@ const registerUser = asyncHandler(async (req, res, next) => {
     } else {
       await User.create(req.body, (err2, newUser) => {
         if (err2) {
-          next(err2);
+          const errors = Object.values(err2.errors).map((error) => {
+            if (error.path === 'password') {
+              return 'Please enter a password of length more than 6 and less than 12 characters';
+            } else {
+              return error.message;
+            }
+          });
+          next(new Error(errors));
         } else {
           res.json({
             _id: newUser._id,
