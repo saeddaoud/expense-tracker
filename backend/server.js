4ac6d99d7858a0b6bf936d2eae2225ba8dcfcb26
Saed Daoud <eng.saed.daoud@gmail.com>
2020-12-25 15:47:08 -0500

    final

--- backend/server.js
+++ backend/server.js
@@ -22,9 +22,9 @@ connectDB();
 // Read static file for deployment
 const __dirname = path.resolve();
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
   app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
   });
 }
 
