2aae993c0f0b2ede7555ddf3f1826b29f513b931
Saed Daoud <eng.saed.daoud@gmail.com>
2021-01-11 16:08:56 -0500

    before adding jumbotron

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,12 @@ import path from 'path';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import colors from 'colors';
+import helmet from 'helmet';
+import cors from 'cors';
+import rateLimit from 'express-rate-limit';
+import hpp from 'hpp';
+import mongoSanitizer from 'express-mongo-sanitize';
+
 import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 import transactionRoutes from './routes/transactionRoutes.js';
 import userRoutes from './routes/userRoutes.js';
@@ -15,6 +21,18 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+
+app.use(helmet());
+app.use(cors());
+app.use(hpp());
+app.use(mongoSanitizer());
+
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 100,
+});
+
+app.use(limiter);
 
 // Connect to DB
 connectDB();
